12df7a33df6bdd8544c9164ad7372060fa499503
pradeepmaurya-neo <pradeep.maurya@neosoftmail.com>
2022-05-16 19:00:18 +0530

    start app1

---
+++ proj_cel_rab/celery.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+
+os.environ.setdefault("DJANGO_SETTING_MODULE", 'proj_cel_rab.settings')
+
+app = Celery('proj_cel_rab')
+
+app.config_from_object('django.conf:settings', namespace="CELERY")
+
+app.autodiscover_tasks
+
